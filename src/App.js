a665ca30f6d1a37493f00184d9138c45c9293ec8
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-07-28 19:56:18 +0300

    Add full logit for Counter Upgrade

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import "./App.css";
+import CounterTop from "./component/Counter/CounterTop";
 
 function App() {
   return (
-    <></>
+    <>
+      <CounterTop />
+    </>
   );
 }
 
 export default App;
+
